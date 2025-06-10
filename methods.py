a455de3177f838c86f2ae55ce61406ee0e65f099
Mark McCarthy <mark.joseph.mccarthy@gmail.com>
2025-06-10 15:00:15 +0100

    operators and methods added

---
+++ methods.py
@@ -0,0 +1,3 @@
+my_text="the quick brown fox jumps over the lazy dog. hello world."
+print(my_text.capitalize())
+print(my_text.strip("."))
---
+++ operators.py
@@ -0,0 +1,20 @@
+print(5+4)
+print(5-4)
+print(5*4)
+print(5/4)
+print(3**2)
+print(9%3)
+
+num1 = 5
+print(f"num1 is currently {num1}")
+# ortiginal way of increment a variable
+num1 = num1 + 2
+# modern way of incrementing a variable is 
+num1 *= 2
+print(f"num1 is now {num1}")
+
+# Can do any of the following:
+# +=
+# -=
+# *=
+# /h
